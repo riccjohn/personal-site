40809aa392561f89768f8a090e3357bde5be8b82
John Riccardi <riccjohn@gmail.com>
2024-05-02 12:25:28 -0500

    Move vitest setup file

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
+    setupFiles: ['.vitest/setup.ts'],
     coverage: {
       thresholds: {
         lines: 90,
